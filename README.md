fd977a9785dcca732c4b73d4ac302c2f61c4142b
sakokazuki <sako@aid-dcc.com>
2018-12-19 17:41:50 +0900

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+## What?
+unityでgrpcクライアント作ったときに勉強したけど結局使わなかったコードを置いています。
+
+- https://qiita.com/mxProject/items/98fb0198b3927dce547c
+- https://qiita.com/mxProject/items/565e8960315b19bcaef4
+
+を合体させて継承じゃなく、intarfaceでobsrevableな共通処理を書くっていう内容  
+
+単体では動作しない。
---
+++ grpc/GrpcServerStreamingObservable.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Grpc.Core;
+using System.Threading.Tasks;
+using System.Threading;
+public sealed class GrpcServerStreamingObservable<TResponse> : IObservable<TResponse>, IDisposable {
+
+	private IAsyncStreamReader<TResponse> m_ResponseStream;
+	private IAsyncResponseStreamingCall<TResponse> m_Call;
+	private bool m_diposableCall;
+
+
+
+	private GrpcServerStreamingObservable(IAsyncResponseStreamingCall<TResponse> call, bool disposableCall){
+		m_Call = call;
+		m_ResponseStream = call.ResponseStream;
+		m_diposableCall = disposableCall;
+	}
+
+	public static GrpcServerStreamingObservable<TResponse> Observe(IAsyncResponseStreamingCall<TResponse> call, bool disposableCall){
+		return new GrpcServerStreamingObservable<TResponse>(call, disposableCall);
+	}
+
+	~GrpcServerStreamingObservable(){
+		Dispose();
+	}
+
+	public void Dispose(){
+		Debug.Log("gsso dispose");	
+		GC.SuppressFinalize(this);
+
+		if(m_diposableCall){
+			m_Call.Dispose();
+		}
+		ReleaseObservers();
+		ReleaseSubscribers();
+	}
+	
+	private readonly List<IObserver<TResponse>> m_Observers = new List<IObserver<TResponse>>();
+
+	public async Task ObserveAsync(CancellationToken cancellation = default(CancellationToken)){
+		try{
+			while(!cancellation.IsCancellationRequested && await m_ResponseStream.MoveNext().ConfigureAwait(false)){
+				OnNext(m_ResponseStream.Current);
+			}
+			Debug.Log("gsso before finally");
+			OnCompleted();
+		}catch(Exception ex){
+			OnError(ex);
+			throw;
+		}finally{
+
+		}
+	}
+
+	private void ReleaseObservers(){
+		lock(m_Observers){
+			m_Observers.Clear();
+		}
+	}
+
+	private void OnCompleted(){
+		Debug.Log("gsso oncompleted count: "+m_Observers.Count);
+		if(m_Observers.Count == 0){
+			return;
+		}
+		lock(m_Observers){
+			if(m_Observers.Count > 0){
+				m_Observers.ForEach(o => o.OnCompleted());
+				m_Observers.Clear();
+			}
+		}
+	}
+
+	private void OnError(Exception error){
+		if(m_Observers.Count == 0){
+			return;
+		}
+		lock(m_Observers){
+			if(m_Observers.Count > 0){
+				m_Observers.ForEach(o => o.OnError(error));
+				m_Observers.Clear();
+			}
+		}
+	}
+
+	private void OnNext(TResponse response){
+		if(m_Observers.Count == 0){
+			return;
+		}
+		lock(m_Observers){
+			if(m_Observers.Count > 0){
+				m_Observers.ForEach(o => o.OnNext(response));
+			}
+		}
+	}
+
+	private readonly List<IDisposable> m_Subscribers = new List<IDisposable>();
+
+	public IDisposable Subscribe(IObserver<TResponse> observer){
+		lock(m_Observers){
+			m_Observers.Add(observer);
+		}
+
+		Action onDispose = ()=>{
+			lock(m_Observers){
+				m_Observers.Remove(observer);
+			}
+		};
+
+		IDisposable subscriber = new Subscriber(onDispose);
+
+		lock(m_Subscribers){
+			m_Subscribers.Add(subscriber);
+		}
+		return subscriber;
+	}
+
+	private void ReleaseSubscribers(){
+		lock(m_Subscribers){
+			m_Subscribers.ForEach(o => o.Dispose());
+			m_Subscribers.Clear();
+		}
+	}
+
+	private sealed class Subscriber : IDisposable{
+		public Subscriber(Action onDispose){
+			m_OnDispose = onDispose;
+		}
+
+		~Subscriber(){
+			Dispose(false);
+		}
+
+		private Action m_OnDispose;
+
+		public void Dispose(){
+			
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		private void Dispose(bool disposing){
+			m_OnDispose();
+		}
+
+
+	}
+}
